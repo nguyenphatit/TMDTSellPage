3de6cde146e3e72b4230871b71ff485b40959060
Phat <nguyenphatit.nl@gmail.com>
2017-12-13 23:19:52 +0700

    update
    chinh sua danh sach khoa hoc

---
+++ TMDTSellPage/src/app/view/pages/doi-mat-khau/doi-mat-khau.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'doi-mat-khau.component.html'
+})
+
+export class DoiMatKhauComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
--- TMDTSellPage/src/app/view/pages/pages-routing.module.ts
+++ TMDTSellPage/src/app/view/pages/pages-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DangKyComponent } from './dang-ky/dang-ky.component';
 import { DangNhapComponent } from './dang-nhap/dang-nhap.component';
 import { QuenMatKhauComponent } from './quen-mat-khau/quen-mat-khau.component';
+import { DoiMatKhauComponent } from './doi-mat-khau/doi-mat-khau.component';
 
 
 const routes: Routes = [
   { path: 'dang-ky', component: DangKyComponent },
   { path: 'dang-nhap', component: DangNhapComponent },
-  { path: 'quen-mat-khau', component: QuenMatKhauComponent }
+  { path: 'quen-mat-khau', component: QuenMatKhauComponent },
+  { path: 'doi-mat-khau', component: DoiMatKhauComponent }
 ];
 
 @NgModule({
