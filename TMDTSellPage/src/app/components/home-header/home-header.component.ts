3067e4398fce5c89c190b01d8b0883411e0bee98
tinhthanh <tinhthanh>
2017-12-14 09:13:30 +0700

    show menu

--- TMDTSellPage/src/app/components/home-header/home-header.component.ts
+++ TMDTSellPage/src/app/components/home-header/home-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-home-header',
@@ -8,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeHeaderComponent implements OnInit {
     isShow = false;
+
+    effectNavbar = false;
+    public megamenu = true; // hien thi menu khi thu nhỏ 
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+     // console.log(event.target.innerWidth);
+      if (event.target.innerWidth < 990 ) {
+          this.megamenu = false ;
+      } else {
+          this.megamenu = true;
+      }
+    }
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll() {
+        const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        if (number > 600) {
+            this.effectNavbar =  true;
+        } else {
+         this.effectNavbar = false;
+        }
+      //  console.log(this.navIsFixed +  ' -'  + number)
+      }
     constructor() { }
 
-    ngOnInit() { }
-}
+    ngOnInit() {
+        if ( window.innerWidth < 990 ) {
+            this.megamenu = false;
+        } else {
+            this.megamenu = true;
+        }
+    }
