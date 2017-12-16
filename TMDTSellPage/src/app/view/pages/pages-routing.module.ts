14aaf41527cb4e8488d4b8daff4613eda85eaee1
Phat <nguyenphatit.nl@gmail.com>
2017-12-17 00:40:50 +0700

    them trang error-404

--- TMDTSellPage/src/app/view/pages/pages-routing.module.ts
+++ TMDTSellPage/src/app/view/pages/pages-routing.module.ts
@@ -4,13 +4,15 @@ import { DangKyComponent } from './dang-ky/dang-ky.component';
 import { DangNhapComponent } from './dang-nhap/dang-nhap.component';
 import { QuenMatKhauComponent } from './quen-mat-khau/quen-mat-khau.component';
 import { DoiMatKhauComponent } from './doi-mat-khau/doi-mat-khau.component';
+import { Error404Component } from './error-404/error-404.component';
 
 
 const routes: Routes = [
   { path: 'dang-ky', component: DangKyComponent },
   { path: 'dang-nhap', component: DangNhapComponent },
   { path: 'quen-mat-khau', component: QuenMatKhauComponent },
-  { path: 'doi-mat-khau', component: DoiMatKhauComponent }
+  { path: 'doi-mat-khau', component: DoiMatKhauComponent },
+  { path: 'error-404', component: Error404Component}
 ];
 
 @NgModule({
